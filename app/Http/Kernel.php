00cb204a995e3edb2c1dbf9d43a84d9549f552f2
satria1697 <andhikasatriab@gmail.com>
2021-06-21 21:27:57 +0700

    make own cors

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -17,8 +17,8 @@ class Kernel extends HttpKernel
      * @var array
      */
     protected $middleware = [
-        // \Fruitcake\Cors\HandleCors::class,
         // \App\Http\Middleware\TrustHosts::class,
+        \App\Http\Middleware\CorsMiddleware::class,
         \App\Http\Middleware\TrustProxies::class,
         \App\Http\Middleware\PreventRequestsDuringMaintenance::class,
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
