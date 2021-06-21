52123ca6e4dc3d607fc7c95285df7851d4fbf97c
satria <andhikasatriab@gmail.com>
2021-06-21 21:25:38 +0700

    implement htaccess

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,5 +1,10 @@
 <?php
 
+header('Access-Control-Allow-Origin', '*');
+header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS');
+header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+
+
 /*
 |--------------------------------------------------------------------------
 | Create The Application
